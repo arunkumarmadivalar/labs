f120594509cd67b43ff72b96d9421bdf53d2e6fc
arunkumarmadivalar <arunmadiwalar135@gmail.com>
2024-11-27 18:07:09 +0530

    Update setupUser.sh

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -4,8 +4,7 @@ now=`date +%d%b%Y-%H%M`
 
 exp()
 {
-	"$1" <(cat <<-EOF
-	spawn passwd $USER
+	"$1" <(cat <<-EOF spawn passwd $USER
 	expect "Enter new UNIX password:"
 	send -- "$passw\r"
 	expect "Retype new UNIX password:"
